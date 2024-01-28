0c255687cceba3ecc220b059088e64b9bb5d8e84
UmitSarica <usarica@gmail.com>
2024-01-28 19:55:21 +0300

    Initial Commit

---
+++ LocRecStory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocRecStory
+//
+//  Created by Umit SARICA on 28.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
